7b5403d3665562fa0524013f824a34a91379b510
MatteoSanson <matteo.sanson@gmail.com>
2024-02-19 23:40:57 +0100

    feat: prove search

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -10,6 +10,18 @@ class ProjectController extends Controller
 {
     public function index() {
 
+        request()->validate([
+            'key' => ['nullable','string', 'min:2']
+        ]);
+
+        // dd(request()->key);
+
+        if(request()->key){
+            $projects = Project::where('title', 'LIKE', '%'. request()->key .'%')->paginate(6);
+        } else {
+            $projects = Project::paginate(6);
+        }
+
         // $projects = Project::all();
         
         // $projects = Project::paginate(9);
